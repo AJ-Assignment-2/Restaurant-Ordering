c4899d8fff6fef7cbc3120a6f5beaba73a113266
Imanuel Halim <imanuelhalim1991@gmail.com>
2018-10-02 00:26:19 +1000

    added a chef screen

---
+++ src/ChefScreen/Run.java
@@ -0,0 +1,16 @@
+package ChefScreen;
+
+/**
+ *
+ * @author Imanuel
+ */
+public class Run {
+    public static void main(String[] args){
+        ChefScreenView chefScreenView=new ChefScreenView();
+        ChefScreenModel chefScreenModel=new ChefScreenModel();
+        ChefScreenController chefScreenController=new ChefScreenController(chefScreenView, chefScreenModel);
+        chefScreenView.setTitle("Chef Screen Status");
+        chefScreenView.setSize(1200,300);
+        chefScreenView.setVisible(true);
+    }
+}
